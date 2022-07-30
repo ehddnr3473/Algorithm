79590095f52f34df1c008dc206e68694f88c6c7a
김동욱 <kimdonguk@gimdong-ug-ui-MacBookAir.local>
2022-07-30 15:43:59 +0900

    수박수박

---
+++ Suback/Suback/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Suback
+//
+//  Created by 김동욱 on 2022/07/30.
+//
+
+import Foundation
+
+func solution(_ n:Int) -> String {
+    var result = [String]()
+    if n % 2 == 0 {
+        for _ in 1...n/2 {
+            result.append("수박")
+        }
+    } else {
+        for _ in 1...n/2 {
+            result.append("수박")
+        }
+        result.append("수")
+    }
+    return result.joined()
+}
+
+print(solution(5))
