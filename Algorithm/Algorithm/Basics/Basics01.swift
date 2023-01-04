c16e38aa4c2c929be2aeba1290da7df29f9cb081
Donguk Kim <ehddnr73@naver.com>
2023-01-04 18:31:45 +0900

    Basics01 Dictionary 사용 예시 추가

--- Algorithm/Algorithm/Basics/Basics01.swift
+++ Algorithm/Algorithm/Basics/Basics01.swift
@@ -9,7 +9,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120894?language=swift
 import Foundation
 
-func solution(_ numbers:String) -> Int64 {
+func solution(_ numbers: String) -> Int64 {
     var numbers = numbers
     var result = ""
     let startIndex = numbers.startIndex
@@ -27,6 +27,20 @@ func solution(_ numbers:String) -> Int64 {
         count = 0
     }
     
+    return Int64(result)!
+}
+
+func solution_2(_ numbers: String) -> Int64 {
+    let numberDictionary = ["zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
+    var result = ""
+    var number = ""
+    for n in numbers {
+        number += String(n)
+        if numberDictionary.keys.contains(number) {
+            result += String(numberDictionary[number]!)
+            number = ""
+        }
+    }
     return Int64(result)!
 }
 
