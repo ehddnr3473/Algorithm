b3743b0295044fa0676a302e990ccaa6d4025a10
Donguk Kim <ehddnr73@naver.com>
2023-01-04 18:15:03 +0900

    문자열 처리

---
+++ Algorithm/Algorithm/Basics/Basics01.swift
@@ -0,0 +1,60 @@
+//
+//  Basics01.swift
+//  Algorithm
+//
+//  Created by 김동욱 on 2023/01/04.
+//
+
+// 프로그래머스, 영어가 싫어요
+// https://school.programmers.co.kr/learn/courses/30/lessons/120894?language=swift
+import Foundation
+
+func solution(_ numbers:String) -> Int64 {
+    var numbers = numbers
+    var result = ""
+    let startIndex = numbers.startIndex
+    var count = 0
+    
+    while numbers.count != 0 {
+        let endIndex = numbers.index(startIndex, offsetBy: count)
+        let numberString = String(numbers[startIndex...endIndex])
+        guard let number = verify(numberString) else {
+            count += 1
+            continue
+        }
+        result += number
+        numbers.removeSubrange(startIndex...endIndex)
+        count = 0
+    }
+    
+    return Int64(result)!
+}
+
+func verify(_ numberString: String) -> String? {
+    switch(numberString) {
+    case "zero":
+        return "0"
+    case "one":
+        return "1"
+    case "two":
+        return "2"
+    case "three":
+        return "3"
+    case "four":
+        return "4"
+    case "five":
+        return "5"
+    case "six":
+        return "6"
+    case "seven":
+        return "7"
+    case "eight":
+        return "8"
+    case "nine":
+        return "9"
+    default:
+        return nil
+    }
+}
+
+//print(solution("onetwothreefourfivesixseveneightnine"))
