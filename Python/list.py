c6ec69a9a159738856aceb2ff1dd54798acaeddd
Donguk Kim <ehddnr73@naver.com>
2023-01-15 15:16:51 +0900

    index 조건 반환

--- Python/list.py
+++ Python/list.py
@@ -2,3 +2,11 @@ array = [0, 1, 2]
 
 print(len(array))
 print(array.count)
+
+
+# index return
+def search_index(n):
+    return array.index(n) if n in array else -1
+
+index = search_index(3)
+print(index)
