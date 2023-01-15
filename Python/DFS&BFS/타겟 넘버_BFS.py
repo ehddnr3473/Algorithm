3fe22574e3a3345752edf2466b6c891ee78145d0
김동욱 <55693272+ehddnr3473@users.noreply.github.com>
2023-01-15 23:45:47 +0900

    타겟 넘버_BFS

---
+++ Python/DFS&BFS/타겟 넘버_BFS.py
@@ -0,0 +1,17 @@
+def solution(numbers, target):
+    answer = 0
+    leaves = [0]
+    for num in numbers:
+        tmp = []
+        for parent in leaves:
+            tmp.append(parent + num)
+            tmp.append(parent - num)
+        leaves = tmp
+    print(leaves)
+    for leaf in leaves:
+        if leaf == target:
+            answer += 1
+    return answer
+
+
+print(solution([4, 1, 2, 1], 2))
