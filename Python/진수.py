0513ef791e80f4271958d913754b043b462fabb5
Donguk Kim <ehddnr73@naver.com>
2023-01-16 16:11:15 +0900

    반복문 출력 예시

--- Python/진수.py
+++ Python/진수.py
@@ -1,8 +1,15 @@
 number = 8
 print(bin(number))
 print(int(0b1000))
-print(format(bin(number), 'b'))
-print(format(bin(number), '#b'))
+print(format(number, 'b'))
+print(format(number, '#b'))
 print(oct(number))
 number = 11
 print(hex(number))
+
+
+numbers = [9, 20, 28, 18, 11]
+
+for i in numbers:
+    row = format(i, 'b')
+    print(row)
