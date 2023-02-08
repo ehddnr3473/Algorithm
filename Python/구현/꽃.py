d6691ed85e6257332c7a1f20879e32dc68ddf195
Donguk Kim <ehddnr73@naver.com>
2023-02-08 21:40:49 +0900

    기존의 중첩 반복문을 사용하여 해결했던 문제를 큐 개념을 도입하여 해결

--- Python/구현/꽃.py
+++ Python/구현/꽃.py
@@ -1,46 +1,55 @@
 #다음과 같이 import를 사용할 수 있습니다.
 #import math
+from collections import deque
 
-def solution(n, garden):
+def solution(garden):
     #여기에 코드를 작성해주세요.
+    n = len(garden)
+    result = 0
     dx = [-1, 0, 1, 0]
     dy = [0, -1, 0, 1]
-    cnt = 0
-    visited = [[False for _ in range(len(garden))] for _ in range(len(garden))]
+    queue = deque()
     
-    while True:
-        flower_cnt = 0
-        for i in garden:
-            flower_cnt += sum(i)          
-        if flower_cnt == len(garden) * len(garden): break
-        
-        for i in range(len(garden)):
-            for j in range(len(garden)):
-                if garden[i][j] == 1 and not visited[i][j]:
-                    visited[i][j] = True
-                    for k in range(4):
-                        nx = i + dx[k]
-                        ny = j + dy[k]
-                        if (nx > -1 and nx < len(garden) and ny > -1 and ny < len(garden)):
-                            garden[nx][ny] = 1
-              
-        cnt += 1
-    return cnt
+    for x in range(n):
+        for y in range(n):
+            if garden[x][y] == 1:
+                queue.append((x, y))
+                
+    while queue:
+        for i in range(len(queue)):
+            x, y = queue.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if (nx > -1 and nx < n and ny > -1 and ny < n) and garden[nx][ny] == 0:
+                    garden[nx][ny] = 1
+                    queue.append((nx, ny))
+        result += 1
+    
+    return result-1
+    
+    
 
 #아래는 테스트케이스 출력을 해보기 위한 코드입니다.
-n1 = 3
 garden1 = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-ret1 = solution(n1, garden1)
+ret1 = solution(garden1)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret1, "입니다.")
 
-n2 = 2
 garden2 = [[1, 1], [1, 1]]
-ret2 = solution(n2, garden2)
+ret2 = solution(garden2)
 
 #[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 print("solution 함수의 반환 값은", ret2, "입니다.")
+
+garden3 = [[0, 0, 0], [0, 0, 0], [1, 0, 0]]
+ret3 = solution(garden3)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret3, "입니다.")
 
 #####매개변수 설명
 # 정원 크기 n과 현재 정원 상태를 담은 2차원 리스트 garden이 solution 함수의 매개변수로 주어집니다.
