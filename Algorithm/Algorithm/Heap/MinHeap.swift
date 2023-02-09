5a87c562b5aea4a4fa6f36148436f20ce25f19ee
Donguk Kim <ehddnr73@naver.com>
2023-02-09 23:47:13 +0900

    minHeap 수정

--- Algorithm/Algorithm/Heap/MinHeap.swift
+++ Algorithm/Algorithm/Heap/MinHeap.swift
@@ -8,7 +8,7 @@
 // Min Heap
 import Foundation
 
-struct MinHeap {
+class MinHeap {
     var heap = [0]
     
     var isEmpty: Bool { return heap.count == 1 }
@@ -25,7 +25,7 @@ struct MinHeap {
         index * 2 + 1
     }
     
-    mutating func push(_ element: Int) {
+    func push(_ element: Int) {
         if heap.count == 1 {
             heap.append(element)
             return
@@ -35,7 +35,7 @@ struct MinHeap {
         restructureForPush()
     }
     
-    mutating func restructureForPush() {
+    func restructureForPush() {
         var index = heap.count - 1
         
         while index != 1 && heap[index] < heap[parentIndex(index)] {
@@ -44,8 +44,8 @@ struct MinHeap {
         }
     }
     
-    mutating func pop() -> Int? {
-        guard heap.count > 1 else { return nil}
+    func pop() -> Int? {
+        guard heap.count > 1 else { return nil }
         
         if heap.count == 2 {
             return heap.removeLast()
@@ -59,7 +59,7 @@ struct MinHeap {
         return maxValue
     }
     
-    mutating func restructureForPop() {
+    func restructureForPop() {
         var index = 1
         while rightChildIndex(of: index) < heap.count {
             if heap[index] <= min(heap[leftChildIndex(of: index)], heap[rightChildIndex(of: index)]) {
